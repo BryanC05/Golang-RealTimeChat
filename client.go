5ce71b119d8acac8d1b8a81e1cc0a818d163afc8
ItzDelay <05bryanchan05@gmail.com>
2025-11-01 08:18:53 +0700

    Refactor WebSocket client implementation

--- client.go
+++ client.go
@@ -9,48 +9,31 @@ import (
 )
 
 const (
-	// Time allowed to write a message to the peer.
 	writeWait = 10 * time.Second
 
-	// Time allowed to read the next pong message from the peer.
 	pongWait = 60 * time.Second
 
-	// Send pings to peer with this period. Must be less than pongWait.
 	pingPeriod = (pongWait * 9) / 10
 
-	// Maximum message size allowed from peer.
 	maxMessageSize = 512
 )
 
-// upgrader specifies the parameters for upgrading an HTTP connection to a
-// WebSocket connection. We use a simple CheckOrigin for this example.
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	// We allow all origins for this simple example.
 	CheckOrigin: func(r *http.Request) bool {
 		return true
 	},
 }
 
-// Client is a middleman between the websocket connection and the hub.
 type Client struct {
 	hub *Hub
-
-	// The websocket connection.
 	conn *websocket.Conn
-
-	// Buffered channel of outbound messages.
 	send chan []byte
-
-	// The client's username
 	username string
 }
 
-// readPump pumps messages from the websocket connection to the hub.
 func (c *Client) readPump() {
-	// This defer ensures that we unregister the client and close the
-	// connection when this goroutine exits (e.g., on error).
 	defer func() {
 		c.hub.unregister <- c
 		c.conn.Close()
@@ -63,9 +46,7 @@ func (c *Client) readPump() {
 		return nil
 	})
 
-	// This is the main read loop for the client.
 	for {
-		// Read a message from the WebSocket
 		_, message, err := c.conn.ReadMessage()
 		if err != nil {
 			if websocket.IsUnexpectedCloseError(err, websocket.CloseGoingAway, websocket.CloseAbnormalClosure) {
@@ -74,8 +55,6 @@ func (c *Client) readPump() {
 			break
 		}
 
-		// We have a new message. Create a Message struct and send it
-		// to the hub's broadcast channel.
 		chatMsg := &Message{
 			Type:      "chat",
 			Username:  c.username,
@@ -86,7 +65,6 @@ func (c *Client) readPump() {
 	}
 }
 
-// writePump pumps messages from the hub to the websocket connection.
 func (c *Client) writePump() {
 	ticker := time.NewTicker(pingPeriod)
 	defer func() {
@@ -94,29 +72,26 @@ func (c *Client) writePump() {
 		c.conn.Close()
 	}()
 
-	// This is the main write loop for the client.
 	for {
 		select {
 		case message, ok := <-c.send:
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if !ok {
-				// The hub closed the channel.
 				c.conn.WriteMessage(websocket.CloseMessage, []byte{})
 				return
 			}
 
-			// Send the message to the client
 			err := c.conn.WriteMessage(websocket.TextMessage, message)
 			if err != nil {
 				return
 			}
 
 		case <-ticker.C:
-			// Send a ping message to keep the connection alive
 			c.conn.SetWriteDeadline(time.Now().Add(writeWait))
 			if err := c.conn.WriteMessage(websocket.PingMessage, nil); err != nil {
 				return
 			}
 		}
 	}
+
 }
